6fd36da49c9ae0dd7d743ab2cc20db451d66b02f
Jordy2010 <jordyeduardosebastianlopez@gmail.com>
2026-04-15 13:01:13 -0600

    Add files via upload

---
+++ tarea_will1.py
@@ -0,0 +1,12 @@
+#Fecha de elaboración:25/03/2026
+#Nombre de alumno:Jordy Eduardo Sebastian Lopez
+#Tarea:Leer dos números enteros  imprimir cuál es mayor
+#SUB 1 PROG 2
+
+num1 = int(input("Ingrese el primer numero"))
+num2 = int(input("Ingrese el segundo numero"))
+
+if num1 >= num2:
+    print(f"El numero mayor es {num1}")
+if num2 >= num1:
+    print(f"El numero mayor es {num2}")
