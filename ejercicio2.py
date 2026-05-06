83c390dbfb9576c4af7d56fac67120bb2ecc02a2
Jordy2010 <jordyeduardosebastianlopez@gmail.com>
2026-05-06 12:47:33 -0600

    Create ejercicio2.py

---
+++ ejercicio2.py
@@ -0,0 +1,12 @@
+#Nombre del alumno:Jordy Eduardo Sebastian Lopez
+#Ejercicio 2:Leer un número y escribir el valor absoluto del mismo
+#Fecha de elaboración:16-04-2026
+
+#Leer un número del usuario
+numero = float(input("Ingrese un número: "))
+
+#Calcular el valor absoluto
+valor_absoluto = abs(numero)
+
+#Mostrar el resultado
+print(f"El valor absoluto es: {valor_absoluto}")
