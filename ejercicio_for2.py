8eb9de286a11555876f45667ed2c1d6f7a8b85a5
Jordy2010 <jordyeduardosebastianlopez@gmail.com>
2026-05-06 20:23:11 -0600

    Create ejercicio_for2.py

---
+++ ejercicio_for2.py
@@ -0,0 +1,13 @@
+#Nombre del alumno:Jordy Eduardo Sebastian Lopez
+#Ejercicio For 2:
+#Fecha de elaboración:24-04-2026
+
+#Ciclo para leeer 10 números
+
+for i in range(10):
+    num = float(input(f"Ingrese el número { i + 1}: "))
+    cubo = num ** 3
+    cuarta = num ** 4
+    print(f"El cubo de {num} es : {cubo}, y su cuarta es: {cuarta}")
+
+
